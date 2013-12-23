fc113501a6fc2d9b3289c80a74f7ffc968f61203
juanvegab <juanvegab23@gmail.com>
2013-12-23 11:54:45 -0600

    new changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,9 @@
 var GetURLParameter = function(sParam){
-	var sPageURL = window.location.search.substring(1);
-	var sURLVariables = sPageURL.split('&');
-	for (var i = 0; i < sURLVariables.length; i++) {
-		var sParameterName = sURLVariables[i].split('=');
-		if (sParameterName[0] == sParam) {
-			return sParameterName[1];
-		}
-	}
+	return decodeURI(
+		(RegExp(sParam + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
+	);
 }
 
 var name = GetURLParameter('name');
 console.log(name);
 $('.card-name').html(name);
-setInterval(function(){
-	$('.card-name').show();
-	setInterval(function(){
-		$('.card-name').hide();
-	}, 17750);
-}, 46000);
